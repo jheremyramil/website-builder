46814024cf2e3e078f72d10c1c28500ba6032d2d
Marc Francisco <marcpfrancisco@gmail.com>
2024-12-11 20:01:49 +0800

    APP:
    - Added color indicator on acitve selected device button

--- frontend/components/Navbar/Devices/index.tsx
+++ frontend/components/Navbar/Devices/index.tsx
@@ -1,30 +1,44 @@
 import { useEditorStore } from "@/store";
 import { MonitorIcon, TabletIcon, SmartphoneIcon } from "lucide-react";
-import React from "react";
+import { useState } from "react";
 
 const Devices = () => {
   const { editor } = useEditorStore();
+  const [activeDevice, setActiveDevice] = useState<string>("desktop");
 
   const setDevice = (device: string) => {
     editor?.Devices.select(device);
+    setActiveDevice(device);
   };
 
   return (
     <div className="flex items-center gap-x-3">
       <button
-        className="rounded-xl p-2 text-blue-600 bg-gray-100"
+        className={`rounded-xl p-2 ${
+          activeDevice === "desktop"
+            ? "text-blue-600 bg-gray-100"
+            : "text-gray-400 hover:bg-gray-100"
+        }`}
         onClick={() => setDevice("desktop")}
       >
         <MonitorIcon className="h-6 w-6 stroke-current" />
       </button>
       <button
-        className="rounded-xl p-2 text-gray-400 hover:bg-gray-100"
+        className={`rounded-xl p-2 ${
+          activeDevice === "tablet"
+            ? "text-blue-600 bg-gray-100"
+            : "text-gray-400 hover:bg-gray-100"
+        }`}
         onClick={() => setDevice("tablet")}
       >
         <TabletIcon className="h-6 w-6 stroke-current" />
       </button>
       <button
-        className="rounded-xl p-2 text-gray-400 hover:bg-gray-100"
+        className={`rounded-xl p-2 ${
+          activeDevice === "mobile"
+            ? "text-blue-600 bg-gray-100"
+            : "text-gray-400 hover:bg-gray-100"
+        }`}
         onClick={() => setDevice("mobile")}
       >
         <SmartphoneIcon className="h-6 w-6 stroke-current" />
